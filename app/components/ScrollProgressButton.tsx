cbd729fa0a5035be13862ef6917b27bd3e320c69
sanjanamasam1210 <sanjanamasam1210@gmail.com>
2025-02-02 22:57:25 +0530

    imported image wasn't used, removed it for deployement issues

--- app/components/ScrollProgressButton.tsx
+++ app/components/ScrollProgressButton.tsx
@@ -1,7 +1,6 @@
 'use client';
 import React, { useState, useEffect } from 'react';
 import Link from 'next/link';
-import Image from 'next/image';
 
 const ScrollProgressButton = () => {
   const [scrollProgress, setScrollProgress] = useState(0);
