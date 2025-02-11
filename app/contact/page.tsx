b4b0471417986aa0029617e18bca30e428dd4cd2
sanjanamasam1210 <sanjanamasam1210@gmail.com>
2025-02-11 14:36:45 +0530

    apostrophe build error solved

--- app/contact/page.tsx
+++ app/contact/page.tsx
@@ -1,27 +1,29 @@
-'use client';
+"use client";
 
-import { useState } from 'react';
+import { useState } from "react";
 
 const Contact = () => {
   const [formData, setFormData] = useState({
-    name: '',
-    email: '',
-    subject: '',
-    message: ''
+    name: "",
+    email: "",
+    subject: "",
+    message: "",
   });
 
   const handleSubmit = (e: React.FormEvent) => {
     e.preventDefault();
     console.log(formData);
-    setFormData({ name: '', email: '', subject: '', message: '' });
+    setFormData({ name: "", email: "", subject: "", message: "" });
   };
 
   return (
-    <div className="flex items-center justify-center px-4"> {/* Center everything in the middle */}
+    <div className="flex items-center justify-center px-4">
+      {" "}
+      {/* Center everything in the middle */}
       <div className="flex flex-col my-28 md:flex-row items-center justify-between w-full max-w-6xl space-y-10 md:space-y-0 md:space-x-16">
         {/* Heading */}
         <h1 className="text-8xl md:text-9xl text-center font-clash-display-bold ">
-          Let's Get in Touch
+          Let&apos;s Get in Touch
         </h1>
 
         {/* Form */}
@@ -32,7 +34,9 @@ const Contact = () => {
               type="text"
               placeholder="FULL NAME"
               value={formData.name}
-              onChange={(e) => setFormData({ ...formData, name: e.target.value })}
+              onChange={(e) =>
+                setFormData({ ...formData, name: e.target.value })
+              }
               className="w-full border-b-2 border-black bg-[#f8f8f8] text-md pb-2"
               required
             />
@@ -45,7 +49,9 @@ const Contact = () => {
                 type="email"
                 placeholder="EMAIL"
                 value={formData.email}
-                onChange={(e) => setFormData({ ...formData, email: e.target.value })}
+                onChange={(e) =>
+                  setFormData({ ...formData, email: e.target.value })
+                }
                 className="w-full border-b-2 border-black bg-[#f8f8f8] text-md pb-2"
                 required
               />
@@ -55,7 +61,9 @@ const Contact = () => {
                 type="text"
                 placeholder="PHONE"
                 value={formData.subject}
-                onChange={(e) => setFormData({ ...formData, subject: e.target.value })}
+                onChange={(e) =>
+                  setFormData({ ...formData, subject: e.target.value })
+                }
                 className="w-full border-b-2 border-black bg-[#f8f8f8] text-md pb-2"
                 required
               />
@@ -67,7 +75,9 @@ const Contact = () => {
             <input
               placeholder="MESSAGE"
               value={formData.message}
-              onChange={(e) => setFormData({ ...formData, message: e.target.value })}
+              onChange={(e) =>
+                setFormData({ ...formData, message: e.target.value })
+              }
               className="w-full border-b-2 border-black bg-[#f8f8f8] text-md pb-2"
               required
             />
